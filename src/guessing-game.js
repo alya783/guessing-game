3f5f7cc9c255cc55000868f9e7db9d66d241c7d6
alya783 <76521831+alya783@users.noreply.github.com>
2021-09-06 17:00:14 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.attempt = Math.round((this.max - this.min) / 2) + this.min;
+        return this.attempt;
     }
 
     lower() {
-
+        this.min = this.attempt + 1;
     }
 
     greater() {
-
+         this.max = this.attempt - 1;
     }
 }
 
